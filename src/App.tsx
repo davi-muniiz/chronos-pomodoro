6b6bcd7b5d2b525f8e7f2dd597c152d2d67a3b83
Davi Muniz <davimunizdasilvaa@gmail.com>
2025-10-16 21:41:20 -0300

    Base CountDown e Form Component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 
 function App() {
@@ -17,6 +19,29 @@ function App() {
 
         <Container>
             <Menu />
+        </Container>
+        <Container>
+            <CountDown />
+        </Container>
+
+        <Container>
+            <form action="" className='form'>
+                <div className="formRow">
+                    <DefaultInput labelText="Task" type='number' id='meuInput' placeholder='Ex: Estudar'></DefaultInput>
+                </div>
+                <div className="formRow">
+                    <p>Nesse ciclo foque por 25 min.</p>
+                </div>
+                
+                <div className="formRow">
+                    <p>Ciclos</p>
+                    <p>0 0 0 0 0 0</p>
+                </div>
+                
+                <div className="formRow">
+                    <button>Enviar</button>
+                </div>
+            </form>
         </Container>
     </>;
 }
